513ae649a2d00032e51d368583936e070c39b81e
skowkil <killer014@wp.pl>
2013-01-20 14:23:19 +0100

    Car

---
+++ Car.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, marka, model, rocznik, przebieg, rodzajPaliwa, cena):
+        self.cmarka = marka
+        self.cmodel = model
+        self.crocznik = rocznik
+        self.cprzebieg = przebieg
+        self.cpaliwo = rodzajPaliwa
+        self.ccena = cena
+
